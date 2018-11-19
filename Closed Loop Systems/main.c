ab51a0f8332808aedd5e426845fc98723960f553
Tim <timothyhollabaugh@gmail.com>
2018-11-18 21:48:51 -0500

    move temperature reading to temp.c

--- Closed Loop Systems/main.c
+++ Closed Loop Systems/main.c
@@ -1,47 +1,7 @@
 #include <msp430.h>
 
 #include "uart.h"
-#include "thermistor_10k.h"
-
-double lookup_temp(int voltage) {
-
-    return thermistor_table[voltage>>2]-273;
-
-    //    int temp_index = voltage / THERMISTOR_STEP_DELTA;
-    //
-    //if (temp_index < 0) {
-    //return 0.0;
-    //} else if (temp_index = THERMISTOR_STEP_MAX) {
-    //return thermistor_table[THERMISTOR_STEP_MAX];
-    //} else if (temp_index > THERMISTOR_STEP_MAX) {
-    //return 0.0;
-    //} else {
-
-    /*   |
-     *   |                             *
-     *   |                        *
-     * T |                   *
-     * e |              *
-     * m |         *
-     * p |    *
-     *   |
-     *   |
-     *   +--------------------------------
-     *            ADC reading
-     */
-
-    //double low_temp = thermistor_table[temp_index];
-    //double high_temp = thermistor_table[temp_index+1];
-    //
-    //double slope = (high_temp - low_temp) / THERMISTOR_STEP_DELTA;
-    //
-    //double offset = slope * (voltage - temp_index * THERMISTOR_STEP_DELTA);
-    //
-    //double temp = low_temp + offset;
-    //
-    //return temp;
-    //}
-}
+#include "temp.h"
 
 void set_pwm(int value) {
     if (value == 0) {
@@ -60,15 +20,6 @@ void set_pwm(int value) {
 
 void setup_watchdog() {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-}
-
-void setup_adc() {
-    ADC12CTL0 = ADC12SHT02 + ADC12ON;         // Sampling time, ADC12 on
-    ADC12CTL1 = ADC12SHP;                     // Use sampling timer
-    ADC12IE = 0x01;                           // Enable interrupt
-    ADC12CTL0 |= ADC12ENC;
-    P6SEL |= 0x01;                            // P6.0 ADC option select
-    P1DIR |= 0x01;                            // P1.0 output
 }
 
 void setup_pwm() {
@@ -103,24 +54,17 @@ int temperature_report = 0;
 int main(void) {
 
     setup_watchdog();
-    setup_adc();
     setup_pwm();
 
-    init_serial();
+    temperature_init();
+    serial_init();
 
-    __bis_SR_register(GIE);     // LPM0, ADC12_ISR will force exit
-
-    int value = 0;
-    int acc = 1;
+    __bis_SR_register(GIE);
 
     while (1) {
-        ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
 
         if (temperature_report) {
-            int adc_value = ADC12MEM0;
-
-            double temp = lookup_temp(adc_value);
-
+            double temp = temperature_read();
             write_dec((int)temp);
         }
 
@@ -207,48 +151,6 @@ int main(void) {
                     break;
             }
         }
-    }
-}
-
-/*
- * ADC Interrupt
- */
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector = ADC12_VECTOR
-__interrupt void ADC12_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-    switch(__even_in_range(ADC12IV,34))
-    {
-        case  0: break;                           // Vector  0:  No interrupt
-        case  2: break;                           // Vector  2:  ADC overflow
-        case  4: break;                           // Vector  4:  ADC timing overflow
-        case  6:                                  // Vector  6:  ADC12IFG0
-                 if (ADC12MEM0 >= 0x7ff)                 // ADC12MEM = A0 > 0.5AVcc?
-                     P1OUT |= BIT0;                        // P1.0 = 1
-                 else
-                     P1OUT &= ~BIT0;                       // P1.0 = 0
-
-                 __bic_SR_register_on_exit(LPM0_bits);   // Exit active CPU
-        case  8: break;                           // Vector  8:  ADC12IFG1
-        case 10: break;                           // Vector 10:  ADC12IFG2
-        case 12: break;                           // Vector 12:  ADC12IFG3
-        case 14: break;                           // Vector 14:  ADC12IFG4
-        case 16: break;                           // Vector 16:  ADC12IFG5
-        case 18: break;                           // Vector 18:  ADC12IFG6
-        case 20: break;                           // Vector 20:  ADC12IFG7
-        case 22: break;                           // Vector 22:  ADC12IFG8
-        case 24: break;                           // Vector 24:  ADC12IFG9
-        case 26: break;                           // Vector 26:  ADC12IFG10
-        case 28: break;                           // Vector 28:  ADC12IFG11
-        case 30: break;                           // Vector 30:  ADC12IFG12
-        case 32: break;                           // Vector 32:  ADC12IFG13
-        case 34: break;                           // Vector 34:  ADC12IFG14
-        default: break;
     }
 }
 
